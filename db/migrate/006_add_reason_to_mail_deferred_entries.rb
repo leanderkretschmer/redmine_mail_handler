1d8cd7fa0632cf5b87521bf2d0fbbd5923187742
leanderkretschmer <leanderkretschmer@icloud.com>
2025-09-18 11:55:42 +0200

    fixed db creation loop

--- db/migrate/006_add_reason_to_mail_deferred_entries.rb
+++ db/migrate/006_add_reason_to_mail_deferred_entries.rb
@@ -1,6 +1,8 @@
 class AddReasonToMailDeferredEntries < ActiveRecord::Migration[5.2]
   def change
-    add_column :mail_deferred_entries, :reason, :string, default: 'unknown_user'
-    add_index :mail_deferred_entries, :reason
+    unless column_exists?(:mail_deferred_entries, :reason)
+      add_column :mail_deferred_entries, :reason, :string, default: 'unknown_user'
+      add_index :mail_deferred_entries, :reason
+    end
   end
 end
