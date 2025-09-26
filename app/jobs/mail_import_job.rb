85d1db1284d049f17e3d829bd29a6d7a2163f8a8
leanderkretschmer <leanderkretschmer@icloud.com>
2025-09-26 15:03:35 +0200

    WIP sidekiq support

--- app/jobs/mail_import_job.rb
+++ app/jobs/mail_import_job.rb
@@ -1,13 +1,21 @@
 class MailImportJob
   include Sidekiq::Job
 
-  sidekiq_options queue: :mail_import, retry: 3
+  # Use default queue unless you configured a dedicated one
+  sidekiq_options queue: :default, retry: 3
 
   def perform(limit = nil)
     settings = Setting.plugin_redmine_mail_handler
     receiving = ENV['WITH_EMAIL_RECEIVING'].to_s.strip.downcase
     receiving_enabled = (receiving == '1' || receiving == 'true' || receiving == 'yes')
-    return unless receiving_enabled && settings['auto_import_enabled'] == '1'
+    unless receiving_enabled
+      Rails.logger.info "[MailHandler] MailImportJob: skipped (WITH_EMAIL_RECEIVING is not enabled)"
+      return
+    end
+    unless settings['auto_import_enabled'] == '1'
+      Rails.logger.info "[MailHandler] MailImportJob: skipped (auto_import_enabled != '1')"
+      return
+    end
 
     logger = MailHandlerLogger.new
     logger.info("Sidekiq MailImportJob started#{limit ? " (limit: #{limit})" : ''}")
