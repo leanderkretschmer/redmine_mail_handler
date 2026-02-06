440b3665c5bbd18d29d220aae5b542f6afa8b42d
leanderkretschmer <106610902+leanderkretschmer@users.noreply.github.com>
2026-02-06 15:09:31 +0100

    bumped version to 0.4.0

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_mail_handler do
   name 'Redmine Mail Handler'
   author 'Leander Kretschmer'
   description 'Erweiterte Mail-Verarbeitung für Redmine mit IMAP-Support, automatischer Ticket-Zuweisung und Reminder-Funktionen'
-  version '0.3.2'
+  version '0.4.0'
   url 'https://github.com/leanderkretschmer/redmine_mail_handler'
   author_url 'https://github.com/leanderkretschmer'
 
